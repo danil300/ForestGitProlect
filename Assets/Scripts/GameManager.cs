cabf5dd70116c11626aaa89e0a2aef92e5c9af58
Danil <danil.gaiko.034@mail.ru>
2025-06-10 17:02:49 +0300

    Revert "1"
    This reverts commit 6202b40d4efb761e8017930761a10aebef3e7461.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,20 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+<<<<<<< HEAD
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-
-
+=======
+>>>>>>> alex_proto
 
 public class GameManager : MonoBehaviour
 {
     public float PlayerHP;
+<<<<<<< HEAD
     public float MaxPlayerHP;
-
+=======
+>>>>>>> alex_proto
     public float PlayerDamage;
     public float CollectItems = 0f;
     public float PlayerMoney;
     public float PlayerOhco;
+<<<<<<< HEAD
     public float MaxKvota;
 
     public Text Item;
@@ -26,22 +30,29 @@ public class GameManager : MonoBehaviour
     private bool ShowLossHp;
     private PlayerController controller;
     private int _Kvota;
+=======
 
-   
+    private GameObject player;
+    private Transform Spawn;
+>>>>>>> alex_proto
 
     public void Start()
     {
         player = GameObject.Find("Player");
         Spawn = GameObject.Find("SpawnPlayer").transform;
+<<<<<<< HEAD
         controller = player.GetComponent<PlayerController>();
         player.transform.position = Spawn.position;
 
+=======
 
         player.transform.position = Spawn.position;
+>>>>>>> alex_proto
     }
 
     public void Update()
     {
+<<<<<<< HEAD
         Item.text = "Предметов:" + CollectItems;
         HP.text = "HP:" + PlayerHP;
         Stamina.text = "" + controller.Stamine;       
@@ -59,5 +70,19 @@ public class GameManager : MonoBehaviour
             SceneManager.LoadScene("SampleScene");
             //вызываем окно смерти
         }
-    }     
+    }
+
+    
+
+  
+=======
+       if (PlayerHP == 0f)
+       {
+            player.transform.position = Spawn.position;
+            PlayerHP += 3;
+            Debug.Log("Игрок погиб");
+            //После поражения игрока телепортирует в лобби, игрок теряет весь набранный лут, а также теряет 1 день
+       }
+    }
+>>>>>>> alex_proto
 }
